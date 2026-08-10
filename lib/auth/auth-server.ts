02839eba7c69a64cf1294182bd1e2cf1debf02f5
zeroequalsone <real.rizzle74@gmail.com>
2026-08-10 03:11:55 +0200

    fix: prevent auth cookie modification

--- lib/auth/auth-server.ts
+++ lib/auth/auth-server.ts
@@ -12,11 +12,7 @@ export const getCurrentUser = async () => {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options),
-          );
-        },
+        setAll() {},
       },
     },
   );
