327832b1da38c0482eb79db42247fefd1bdef8ce
zeroequalsone <real.rizzle74@gmail.com>
2026-08-11 23:51:08 +0200

    feat: add design header

--- components/(dashboard)/user/design/DashboardDesignContent.tsx
+++ components/(dashboard)/user/design/DashboardDesignContent.tsx
@@ -1,3 +1,9 @@
+import DashboardDesignHeader from "./DashboardDesignHeader";
+
 export default function DashboardDesignContent() {
-  return <div></div>;
+  return (
+    <div className="w-full px-10 pt-8 pb-15">
+      <DashboardDesignHeader />
+    </div>
+  );
 }
