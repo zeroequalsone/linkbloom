c3e7e85559cc5d94e027e3e5a5909e4d346b7e47
zeroequalsone <real.rizzle74@gmail.com>
2026-08-12 00:18:06 +0200

    feat: add design theme section

--- components/(dashboard)/user/design/DashboardDesignContent.tsx
+++ components/(dashboard)/user/design/DashboardDesignContent.tsx
@@ -1,9 +1,11 @@
 import DashboardDesignHeader from "./DashboardDesignHeader";
+import DashboardDesignTheme from "./DashboardDesignTheme";
 
 export default function DashboardDesignContent() {
   return (
     <div className="w-full px-10 pt-8 pb-15">
       <DashboardDesignHeader />
+      <DashboardDesignTheme />
     </div>
   );
 }
---
+++ components/(dashboard)/user/design/DashboardDesignTheme.tsx
@@ -0,0 +1,43 @@
+import ThemeButton from "./ThemeButton";
+
+export default function DashboardDesignTheme() {
+  return (
+    <div className="bg-cream-2 p-6 rounded-xl mb-5.5">
+      <div>
+        <p className="text-lg font-fraunces font-semibold">Theme</p>
+        <p className="font-light text-cream-4 text-sm mb-4">
+          Wähl eine Farbstimmung für deine Seite.
+        </p>
+      </div>
+      <div className="grid lg:grid-cols-4 grid-cols-2 gap-4">
+        <ThemeButton
+          nameOfTheme="Blüte"
+          firstColor="bg-cream-1"
+          secondColor="bg-cream-2"
+          thirdColor="bg-cream-4"
+        />
+
+        <ThemeButton
+          nameOfTheme="Wiese"
+          firstColor="bg-mint-1"
+          secondColor="bg-cream-1"
+          thirdColor="bg-mint-4"
+        />
+
+        <ThemeButton
+          nameOfTheme="Karamell"
+          firstColor="bg-cream-2"
+          secondColor="bg-cream-1"
+          thirdColor="bg-cream-5"
+        />
+
+        <ThemeButton
+          nameOfTheme="Morgentau"
+          firstColor="bg-cream-1"
+          secondColor="bg-mint-1"
+          thirdColor="bg-mint-3"
+        />
+      </div>
+    </div>
+  );
+}
