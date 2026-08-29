0f5d2c85e6a0b66ce9feb243dc34e09f9c679184
zeroequalsone <real.rizzle74@gmail.com>
2026-08-29 09:02:32 +0200

    fix(profile): remove @ from profile URL

--- components/(dashboard)/user/links/ViewUserPage.tsx
+++ components/(dashboard)/user/links/ViewUserPage.tsx
@@ -8,7 +8,7 @@ export default async function ViewUserPage() {
 
   return (
     <Link
-      href={`@${username}`}
+      href={username}
       className="hover:bg-cream-3 hover:border-transparent active:bg-cream-3/50 hover:text-white flex items-center gap-2 border border-cream-4 text-cream-5 py-2.5 px-4.5 rounded-full text-sm font-medium cursor-pointer"
     >
       <FiEye />
