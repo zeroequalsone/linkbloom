f7f7697a8be3a76bb28dd2115f26e778e36ac838
zero <98187128+zeroequalsone@users.noreply.github.com>
2026-09-21 23:18:12 +0200

    fix(dashboard-links): fix dashboard links overlap (#25)

--- components/(dashboard)/user/links/DashboardHeader.tsx
+++ components/(dashboard)/user/links/DashboardHeader.tsx
@@ -2,12 +2,12 @@ import ViewUserPage from "./ViewUserPage";
 
 export default function DashboardHeader() {
   return (
-    <div className="flex justify-between items-center mb-7">
+    <div className="flex md:flex-row flex-col justify-between gap-4 md:items-center mb-7">
       <div>
         <p className="text-3xl font-fraunces font-semibold mb-1.5">
           Deine Links
         </p>
-        <p className="font-light text-cream-4">
+        <p className="text-sm font-light text-cream-4">
           Änderungen erscheinen sofort in der Vorschau.
         </p>
       </div>
